5be3b22f049bebc5fbe010f8abccaf08a478aa67
sattamap <sattamap@gmail.com>
2023-12-11 10:17:58 +0600

    Implemented React Router with basic routing structure: Main layout component, Home page, and corresponding routes

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h2>This is my portfolio page</h2>
+        </div>
+    );
+};
+
+export default Portfolio;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,32 @@
+import {
+    createBrowserRouter,
+  
+  } from "react-router-dom";
+  import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import Portfolio from "../pages/Portfolio/Portfolio";
+ 
+
+  
+  
+  
+  
+  
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "/portfolio",
+          element:<Portfolio></Portfolio>,
+        },
+   
+      ]
+    }
+  
+  ]);
